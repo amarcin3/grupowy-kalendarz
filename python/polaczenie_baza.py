971510345add3a171bacdd4135ec1eac44e047d3
iadalbert <129655210+iAdalbert@users.noreply.github.com>
2023-11-06 19:50:32 +0100

    Update polaczenie_baza.py

--- python/polaczenie_baza.py
+++ python/polaczenie_baza.py
@@ -1,47 +1,14 @@
-# Import Firebase REST API library
 import firebase
-
-# Firebase configuration
-config = {
-   "apiKey": "apiKey",
-   "authDomain": "projectId.firebaseapp.com",
-   "databaseURL": "https://databaseName.firebaseio.com",
-   "projectId": "projectId",
-   "storageBucket": "projectId.appspot.com",
-   "messagingSenderId": "messagingSenderId",
-   "appId": "appId"
-}
-
-# Instantiates a Firebase app
-app = firebase.initialize_app(config)
-
-
-# Firebase Authentication
-auth = app.auth()
-
-# Create new user and sign in
-auth.create_user_with_email_and_password(email, password)
-user = auth.sign_in_with_email_and_password(email, password)
-
-
-# Firebase Realtime Database
-db = app.database()
-
-# Data to save in database
-data = {
-   "name": "Robert Downey Jr.",
-   "email": user.get('email')
-}
-
-# Store data to Firebase Database
-db.child("users").push(data, user.get('idToken'))
-
-
-# Firebase Storage
-storage = app.storage()
-
-# File to store in storage
-file_path = 'static/img/example.png'
-
-# Store file to Firebase Storage
-storage.child(user.get('email')).child('uploaded-picture.png').put(file_path, user.get('idToken'))
+if __name__ == '__main__':
+   config = {
+      "apiKey": "AIzaSyD6-UzUyfdk8YXxlncrYTRmdz1FudYs26Y",
+      "authDomain": "grupowy-kalendarz.firebaseapp.com",
+      "projectId": "grupowy-kalendarz",
+      "storageBucket": "grupowy-kalendarz.appspot.com",
+      "messagingSenderId": "257707037636",
+      "appId": "1:257707037636:web:beb7c0031c3d9d64b40a91",
+      "databaseURL": ""
+   }
+   app = firebase.initialize_app(config)
+   db = app.firestore()
+   print(db.collection("Wydarzenia").get())
