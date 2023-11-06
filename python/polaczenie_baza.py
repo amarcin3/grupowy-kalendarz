d98065f2a1949aa371ea16f5b500526021292e24
iadalbert <129655210+iAdalbert@users.noreply.github.com>
2023-11-06 21:23:48 +0100

    Update polaczenie_baza.py

--- python/polaczenie_baza.py
+++ python/polaczenie_baza.py
@@ -1,4 +1,6 @@
 import firebase
+from InquirerPy import prompt
+
 if __name__ == '__main__':
    config = {
       "apiKey": "AIzaSyD6-UzUyfdk8YXxlncrYTRmdz1FudYs26Y",
@@ -11,4 +13,50 @@ if __name__ == '__main__':
    }
    app = firebase.initialize_app(config)
    db = app.firestore()
-   print(db.collection("Wydarzenia").get())
+   
+   Token = "MZgK1Q25kDoggHrg61Ke"
+   userUid = ""
+   events = []
+   '''
+   print(db.collection("Users").get())
+   lista_uzytkownikow = db.collection("Users").list_of_documents()
+   for uzytkownik in lista_uzytkownikow:
+      print(db.collection("Users").document(uzytkownik).get(field_paths=['Imie', 'Nazwisko']))
+   '''
+   def getUrzadzenieData():
+      global userUid
+      dane_urzadenia = db.collection("Urzadzenia").document(Token).get()
+      userUid = dane_urzadenia['Uid']
+   
+   def getUserData():
+      global userUid
+      getUrzadzenieData()
+      dane_uzytkownika = db.collection("Users").document(userUid).get()
+   def getUserEvents():
+      global userUid
+      global events
+      getUrzadzenieData()
+      events = db.collection("Wydarzenia").where("IdOsob", "array_contains", userUid).get()
+      
+   getUserData()
+   getUserEvents()
+   nazwy_wydarzen = []
+   for indeks, wydarzenie in enumerate(events):
+      tytul = ""
+      for key, value in wydarzenie.items():
+         if 'Tytul' in value:
+            nazwy_wydarzen.append(str(indeks+1) + "). " + value['Tytul'])
+
+   questions = [
+      {"type": "checkbox",
+      "message": "Wybierz wydarzenia, na które chcesz reagować",
+      "choices": nazwy_wydarzen},
+    ]
+   result = prompt(questions)
+
+   selectedEvents = []
+   for event in result[0]:
+      selectedEvents.append(int(event.split(")")[0])-1)
+
+   for event in selectedEvents:
+      print(events[event])
